a5ce612a233f41999c47241ba1c772ae7691cba8
DeepKumar <deepkumar-r001@webreinvent.com>
2023-04-06 17:44:37 +0530

    create band page using CMS and display dynamic content

--- VaahCms/Themes/BlogTheme/Resources/views/frontend/band.blade.php
+++ VaahCms/Themes/BlogTheme/Resources/views/frontend/band.blade.php
@@ -27,34 +27,7 @@
 
     <div id="navbarExampleTransparentExample" class="navbar-menu">
         <div class="navbar-start">
-            <a class="navbar-item" href="https://bulma.io/">
-                Home
-            </a>
-            <a class="navbar-item" href="https://bulma.io/">
-                Band
-            </a>
-            <a class="navbar-item" href="https://bulma.io/">
-                Tour
-            </a>
-            <a class="navbar-item" href="https://bulma.io/">
-                Contact
-            </a>
-            <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link" href="/documentation/overview/start/">
-                    More
-                </a>
-                <div class="navbar-dropdown is-boxed">
-                    <a class="navbar-item" href="/documentation/overview/start/">
-                        Mechandise
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
-                        Extras
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                        Media
-                    </a>
-                </div>
-            </div>
+            {!! (vh_location('top', true)) !!}
         </div>
 
         <div class="navbar-end">
@@ -68,250 +41,117 @@
         </div>
     </div>
 </nav>
-{{--{!! (vh_location('top', true)) !!}--}}
+
 
 <section class="hero">
     <figure class="image">
         <img src="https://images.pexels.com/photos/167636/pexels-photo-167636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
 {{--        {!! get_field($data, 'image', 'default') !!}--}}
+{{--        {!! dd(get_field($data, 'image', 'default')) !!}--}}
+{{--        {!! dd(get_field($data, 'image', 'default')) !!}--}}
     </figure>
 </section>
 
 <section class="section">
     <div class="container has-text-centered">
         {!! get_field($data, 'title', 'default','template') !!}
-        {!! get_field($data, 'description', 'default') !!}
+
+        @foreach(get_the_field($data, 'description', 'default','content' , 0, 2) as $index=>$item)
+            @if($index == 0)
+                <p>{{$item}}</p>
+            @endif
+        @endforeach
+{{--        {!! dd(get_field($data, 'description', 'default','content' , 0, 1)) !!}--}}
 
         <div class="columns is-centered" style="padding: 2rem">
-{{--            @foreach( get_the_group($data ,'card' ) as $v)--}}
-{{--            <div class="column">--}}
-{{--                <div class="card">--}}
-{{--                    <div class="card-image">--}}
-{{--                        <figure class="image is-2by1">--}}
-{{--                            <img src="https://images.pexels.com/photos/167635/pexels-photo-167635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Placeholder image">--}}
-{{--                        </figure>--}}
-{{--                    </div>--}}
-{{--                    <div class="card-content">--}}
-{{--                        <div class="media">--}}
-{{--                            <div class="media-content">--}}
-{{--                                <p class="title is-4">{{$v}}</p>--}}
-{{--                                <p class="subtitle is-6">@johnsmith</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="content">--}}
-{{--                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.--}}
-{{--                            <a>@bulmaio</a>.--}}
-{{--                            <a href="#">#css</a>--}}
-{{--                            <a href="#">#responsive</a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            @endforeach--}}
-            <div class="column">
-                <div class="card">
-                    <div class="card-image">
-                        <figure class="image is-2by1">
-                            <img src="https://images.pexels.com/photos/167635/pexels-photo-167635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Placeholder image">
-                        </figure>
-                    </div>
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-content">
-                                <p class="title is-4">John Smith</p>
-                                <p class="subtitle is-6">@johnsmith</p>
-                            </div>
+            @foreach( get_the_group($data ,'card' ) as $v)
+                <div class="column">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-2by1">
+                                <img src="https://images.pexels.com/photos/167635/pexels-photo-167635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Placeholder image">
+                            </figure>
                         </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-content">
+                                    <p class="title is-4">{{$v['heading']}}</p>
+                                    <p class="subtitle is-6">{{$v['subheading']}}</p>
+                                </div>
+                            </div>
 
-                        <div class="content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                            <a>@bulmaio</a>.
-                            <a href="#">#css</a>
-                            <a href="#">#responsive</a>
+                            <div class="content">
+                                {{$v['content']}}
+                                <a>{{$v['a1']}}</a>.
+                                @foreach($v['a2'] as $a)
+                                <a href="#">{{$a}}</a>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="column">
-                <div class="card">
-                    <div class="card-image">
-                        <figure class="image is-2by1">
-                            <img src="https://images.pexels.com/photos/167635/pexels-photo-167635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Placeholder image">
-                        </figure>
-                    </div>
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-content">
-                                <p class="title is-4">John Smith</p>
-                                <p class="subtitle is-6">@johnsmith</p>
-                            </div>
-                        </div>
-
-                        <div class="content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                            <a>@bulmaio</a>.
-                            <a href="#">#css</a>
-                            <a href="#">#responsive</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="card">
-                    <div class="card-image">
-                        <figure class="image is-2by1">
-                            <img src="https://images.pexels.com/photos/167635/pexels-photo-167635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Placeholder image">
-                        </figure>
-                    </div>
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-content">
-                                <p class="title is-4">John Smith</p>
-                                <p class="subtitle is-6">@johnsmith</p>
-                            </div>
-                        </div>
-
-                        <div class="content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                            <a>@bulmaio</a>.
-                            <a href="#">#css</a>
-                            <a href="#">#responsive</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>
 
-{{--@foreach(get_the_group($data ,'card' ) as $v)--}}
-{{--          {{$v}}--}}
-{{--@endforeach--}}
-
-{{--{!! get_group($data ,'card' ) !!}--}}
-
 <section class="section">
     <div class="container has-text-centered">
-        {!! get_field($data, 'heading', 'default') !!}
+        <h2 class="title">
+            @foreach(get_the_field($data, 'heading', 'default') as $index=>$item)
+                @if($index == 0)
+                    {{$item}}
+                @endif
+            @endforeach
+        </h2>
 
         <div class="tile is-ancestor">
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
+            @foreach(get_the_group($data ,'tour' ) as $index=>$tour)
+                @if($index > 4)
+                    @break;
+                @endif
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <figure class="image">
+                            <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+                        </figure>
+                        <p class="title">{{$tour['title']}}</p>
+                        <p class="subtitle">{{$tour['subtitle']}}</p>
+                        <a class="button">{{$tour['button']}}</a>
+                    </article>
+                </div>
+            @endforeach
         </div>
 
         <div class="tile is-ancestor">
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
-            <div class="tile is-parent">
-                <article class="tile is-child box">
-                    <figure class="image">
-                        <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-                    </figure>
-                    <p class="title">New York</p>
-                    <p class="subtitle">Fri 27 Nov 2016</p>
-                    <a class="button">Buy Tickets</a>
-                </article>
-            </div>
+            @foreach(get_the_group($data ,'tour' ) as $index=>$tour)
+                @if($index >= 5 && $index < 10)
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <figure class="image">
+                                <img src="https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+                            </figure>
+                            <p class="title">{{$tour['title']}}</p>
+                            <p class="subtitle">{{$tour['subtitle']}}</p>
+                            <a class="button">{{$tour['button']}}</a>
+                        </article>
+                    </div>
+                @endif
+            @endforeach
         </div>
     </div>
 </section>
 
-{{--@foreach(get_the_group($data ,'tour' ) as $v)--}}
-{{--    {{$v}}--}}
-{{--@endforeach--}}
-{{--{!! get_group($data ,'tour' ) !!}--}}
-{{--{!! get_group($data ,'tour' ,'content' ,0 ) !!}--}}
-
 <section class="section">
     <div class="container has-text-centered">
-        <h2 class="title">Contact</h2>
+        <h2 class="title">
+            @foreach(get_the_field($data, 'heading', 'default') as $index=>$item)
+                @if($index == 1)
+                    {{$item}}
+                @endif
+            @endforeach
+        </h2>
+{{--        {!! dd(get_field($data, 'heading', 'default','content' , 0, 1)) !!}--}}
 
         <form>
             <div class="field is-horizontal">
@@ -324,6 +164,7 @@
                                 </span>
                         </p>
                     </div>
+
                     <div class="field">
                         <p class="control has-icons-left has-icons-right">
                             <input class="input" type="email" placeholder="Email">
@@ -350,7 +191,11 @@
                     <div class="field">
                         <div class="control">
                             <button class="button is-primary">
-                                Send message
+                                @foreach(get_the_field($data, 'description', 'default','content' , 0, 2) as $index=>$item)
+                                    @if($index == 1)
+                                        {{$item}}
+                                    @endif
+                                @endforeach
                             </button>
                         </div>
                     </div>
