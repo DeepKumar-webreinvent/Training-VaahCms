1994e234c6a68bbcadd2b361018f0aab69a4e402
DeepKumar <deepkumar-r001@webreinvent.com>
2023-04-12 18:13:44 +0530

    optimization of tour code

--- VaahCms/Themes/BlogTheme/Resources/views/frontend/pages/touritem.blade.php
+++ VaahCms/Themes/BlogTheme/Resources/views/frontend/pages/touritem.blade.php
@@ -4,12 +4,12 @@
         </h2>
 
 
-        <div class="tile is-ancestor">
-            @foreach(get_the_contents('tour')['list'] as $index=>$tour_item)
-
-                @if($index > 4)
-                    @break;
-                @endif
+        @foreach(get_the_contents('tour')['list'] as $index=>$tour_item)
+          @if($index == 0 || $index == 5)<div class="tile is-ancestor">@endif
+{{--            @foreach(get_the_contents('tour')['list'] as $index=>$tour_item)--}}
+{{--                @if($index > 4)--}}
+{{--                    @break;--}}
+{{--                @endif--}}
 
                 <div class="tile is-parent">
                     <article class="tile is-child box">
@@ -22,28 +22,27 @@
                     </article>
 
                 </div>
-            @endforeach
-        </div>
+{{--            @endforeach--}}
+                @if($index == 4 || $index == 9)</div>@endif
+        @endforeach
 
-        <div class="tile is-ancestor">
-            @foreach(get_the_contents('tour')['list'] as $index=>$tour_item)
 
-                @if($index >= 5 && $index < 10)
-
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            <figure class="image">
-                                <img src="{{get_the_field($tour_item, 'image', 'default')}}">
-                            </figure>
-                            <p class="title">{{get_the_field($tour_item, 'title', 'default')}}</p>
-                            <p class="subtitle">{{get_the_field($tour_item, 'subtitle', 'default')}}</p>
-                            <a class="button">{{get_the_field($tour_item, 'button', 'default')}}</a>
-                        </article>
-
-                    </div>
-                @endif
-            @endforeach
-        </div>
+{{--        <div class="tile is-ancestor">--}}
+{{--            @foreach(get_the_contents('tour')['list'] as $index=>$tour_item)--}}
+{{--                @if($index >= 5 && $index < 10)--}}
+{{--                    <div class="tile is-parent">--}}
+{{--                        <article class="tile is-child box">--}}
+{{--                            <figure class="image">--}}
+{{--                                <img src="{{get_the_field($tour_item, 'image', 'default')}}">--}}
+{{--                            </figure>--}}
+{{--                            <p class="title">{{get_the_field($tour_item, 'title', 'default')}}</p>--}}
+{{--                            <p class="subtitle">{{get_the_field($tour_item, 'subtitle', 'default')}}</p>--}}
+{{--                            <a class="button">{{get_the_field($tour_item, 'button', 'default')}}</a>--}}
+{{--                        </article>--}}
+{{--                    </div>--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
 
     </div>
 </section>
